5c430e4e617f37b08b857b2dcd69c539a668bb3e
Spheniscine <spheniscine@gmail.com>
2025-03-05 13:25:47 +0800

    viewport testing

---
+++ src/components/hero.rs
@@ -0,0 +1,29 @@
+use dioxus::prelude::*;
+
+
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+#[component]
+pub fn Hero() -> Element {
+    // let opts = katex::Opts::builder().output_type(katex::OutputType::Html).build().unwrap();
+    let html = katex::render(r#"1 \large\frac {12} {34} \Omega"#).unwrap();
+
+    rsx! {
+        div {
+            id: "hero",
+            img { src: HEADER_SVG, id: "header" }
+            div { id: "links",
+                span {
+                    class: "select-none",
+                    dangerous_inner_html: {html}
+                }
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use dioxus::prelude::*;
+
+use components::Hero;
+
+mod components;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        // Global app resources
+        document::Link {
+            rel: "stylesheet",
+            href: "https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css",
+            integrity: "sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib",
+            crossorigin: "anonymous"
+        }
+
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        Hero {}
+    }
+}
