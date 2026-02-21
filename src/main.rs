b7f467a812f071a8a8c06a8a79728a0944f35ac5
Spheniscine <spheniscine@gmail.com>
2026-02-21 10:01:29 +0800

    attempt to remove FOUC

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ mod utils;
 mod game;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const MAIN_CSS: &str = include_str!("../assets/styling/main.css");
+const TAILWIND_CSS: &str = include_str!("../assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
@@ -31,8 +31,8 @@ fn App() -> Element {
         }
 
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Style { {MAIN_CSS} }
+        document::Style { {TAILWIND_CSS} }
 
         Hero {}
     }
